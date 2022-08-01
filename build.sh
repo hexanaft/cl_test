2af98fe9618020aebd26d3f530485bcbd1e326e7
Vitaliy Suchkov <vitmeat@gmail.com>
2022-08-01 17:10:10 +0000

    build all

--- build.sh
+++ build.sh
@@ -10,7 +10,21 @@ set -e
 rm -rf out
 mkdir out
 cd out
-# x86_64-alpine-linux-musl-clang++ -o testcpp ../test.cpp -stdlib=libc++ -static-libstdc++ -static --verbose
+
 x86_64-alpine-linux-musl-clang -O3 -g0 -o test1 ../test.c -static
 strip test1 -v
 file test1
+
+x86_64-alpine-linux-musl-clang -O3 -g0 -o zpipe ../zpipe.c -lz -static 
+strip zpipe -v
+file zpipe
+
+x86_64-alpine-linux-musl-clang++ -O3 -g0 -o testcpp ../test.cpp -stdlib=libstdc++ -static-libstdc++ -static
+strip testcpp -v
+file testcpp
+
+x86_64-alpine-linux-musl-clang++ -O3 -g0 -std=c++17 -o test17 ../test17.cpp -stdlib=libstdc++ -static-libstdc++ -static
+strip test17 -v
+file test17
+
+ls -lha
